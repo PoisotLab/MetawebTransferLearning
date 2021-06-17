a6d6b85d389f1892310d51a236250b372d146554
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-06-17 11:40:12 -0400

    update the GLOBI script

--- 05_globi.jl
+++ 05_globi.jl
@@ -1,4 +1,5 @@
 using HTTP
+using ProgressMeter
 using JSON
 using DataFrames
 using CSV: CSV
@@ -11,10 +12,23 @@ canmet = DataFrame(CSV.File("artifacts/canadian_corrected.csv"))
 
 allsp = unique(vcat(canmet.from, canmet.to))
 
-url = "$(_globi_api)/$(rand(allsp))/eats/"
-
-diet = DataFrame(; from=String[], to=String[], type=String[])
-
-r = HTTP.request("GET", url; verbose=3)
-globidiet = JSON.parse(String(r.body))
-
+diet = DataFrame(; from=String[], to=String[])
+@showprogress for sp in allsp
+    url = "$(_globi_api)/$(sp)/eats/"
+    r = HTTP.request("GET", url)
+    globidiet = JSON.parse(String(r.body))
+    if !isempty(globidiet["data"])
+        for intlist in globidiet["data"]
+            if intlist[2] in relevant_types
+                for s in intlist[3]
+                    if s in allsp
+                        if s != sp
+                            push!(diet, (sp, s))
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+diet = unique(diet)
