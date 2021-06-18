51b918c602036009d4341a8594b75c2fa76ee98d
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-06-18 12:50:24 -0400

    update the globi scrit to check missed interactions

--- 05_globi.jl
+++ 05_globi.jl
@@ -3,6 +3,10 @@ using ProgressMeter
 using JSON
 using DataFrames
 using CSV: CSV
+using StatsPlots
+
+theme(:mute)
+default(; frame=:box)
 
 # API info
 _globi_api = "https://api.globalbioticinteractions.org/taxon"
@@ -33,19 +37,30 @@ diet = DataFrame(; from=String[], to=String[])
 end
 diet = unique(diet)
 
-intcode = canmet.from.*canmet.to
-diet.intcode = diet.from.*diet.to
+sort!(diet, [:from, :to])
+CSV.write("artifacts/globi_diet.csv", diet)
 
-missedint = select(diet[findall([!(d in intcode) for d in diet.intcode]),:], [:from, :to])
+intcode = canmet.from .* canmet.to
+diet.intcode = diet.from .* diet.to
+
+missedint = select(diet[findall([!(d in intcode) for d in diet.intcode]), :], [:from, :to])
 sort!(missedint, [:from, :to])
 CSV.write("artifacts/globi_newinteractions.csv")
 
-aug = leftjoin(missedint, canmet; on = [:from, :to])
+matchedint = dropmissing(leftjoin(diet, canmet; on=[:from => :from, :to => :to]))
+
+density(matchedint.score; dpi=600, size=(500, 500), lab="GLOBI", fill=(0.2, 0), lw=0.0)
+density!(canmet.score; ls=:dot, lw=2, c=:grey, lab="All predictions")
+xaxis!("Imputed probability", (0, 1))
+yaxis!("Density", (0, 8))
+savefig("figures/globi-comparison.png")
+
+aug = leftjoin(missedint, canmet; on=[:from, :to])
 aug.score .= 1.0
 
 inflated = vcat(canmet, aug)
 
-sort!(inflated, [:score, :from, :to], rev=[true, false, false])
+sort!(inflated, [:score, :from, :to]; rev=[true, false, false])
 
-# Save the corrected network 
+# Save the corrected network
 CSV.write("artifacts/canadian_inflated.csv", inflated)
