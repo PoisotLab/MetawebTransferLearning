d4b65b8a3ceaeda1cccdec6fea17442880a84b02
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-07-22 10:23:56 -0400

    update the 00md file

--- 00_match_names.jl
+++ 00_match_names.jl
@@ -1,7 +1,16 @@
 # # Step 1 - name matching
 #
 # The names in the parts of the datasets are using different versions of the
-# taxonomy, and so we will reconcile everything using GBIF.
+# taxonomy, and so we will reconcile everything using GBIF. The process is
+# entirely automated, and required no human decision. We rely on a mix of strict
+# matching, and then search through synonyms.
+
+
+# ## Dependencies
+#
+# Nothing here is out of the extroardinary - we do some processing on multiple
+# threads, and use `ProgressMeter` to provide visual feedback on the time to
+# completion.
 
 using Phylo
 using GBIF
@@ -11,14 +20,41 @@ using ProgressMeter
 using DelimitedFiles
 using Base.Threads
 
+# ## Cleaning the European metaweb
+
+# This step is extracting the species names from the species list in the
+# European metaweb - we will need to go through two steps: removing the
+# non-mammals by filtering on the species code, and then extracting the names
+# these species were given in the European metaweb.
 metaweb_names = String.(readdlm(joinpath("data", "Spp_Id.txt")))
 mammal_positions = findall(startswith("M"), metaweb_names[:, 1])
+
+# This line specifically is going to create the list of mammal names are
+# recorded in the European metaweb. As such, this is the first one we will
+# clean.
 mammal_names = metaweb_names[mammal_positions, 2]
+
+# To ensure that we can join dataframes together, we will create a data frame
+# with the species code, the name and ID of the matched species in the GBIF
+# backbone, and finally, a flag to check that the two names are equal. This flag
+# is useful for manual inspection; most often, names are unequal because te
+# European metaweb uses deprecated taxonomic names.
+
+# Note that we are actually creating *one* dataframe per thread. This avoids
+# several threads pushing to the same position at the dataframe, which always
+# results in an error. There are, of course, other ways to avoid the issue, but
+# this one is simple and takes a single line to reconcile.
 
 gbif_cleanup_components = [
     DataFrame(; code=String[], gbifname=String[], gbifid=Int64[], equal=Bool[]) for
     i in 1:nthreads()
 ]
+
+# With the dataframe stores in place, we can distribute the name reconciliation
+# on the different threads. The core of this loop is a search on the GBIF web
+# API; this step is therefore going to be limited by the responses of GBIF. Note
+# that although we use unstrict search (to allow for synonyms, etc), we restrict
+# the search to *Mammalia*.
 
 p = Progress(length(mammal_names))
 @threads for i in 1:length(mammal_names)
@@ -35,16 +71,50 @@ p = Progress(length(mammal_names))
     next!(p)
 end
 
+# Data frames are now filled, and we need to bring them together in a single one
+# -- this is a one-line call to `vcat`, as we simply need to stack them
+# vertically.
+
 gbif_cleanup = vcat(gbif_cleanup_components...)
 
+# ## Clean the GBIF matches
+
+# Names in the European metaweb that are not matched are removed as a
+# precaution:
+
 select!(gbif_cleanup, Not(:equal))
+
+# We finally rename the columns, to facilitate the joins later in the pipeline:
+
 rename!(gbif_cleanup, :code => :metaweb)
 rename!(gbif_cleanup, :gbifname => :name)
 rename!(gbif_cleanup, :gbifid => :id)
+
+# We save the file in the `artifacts` folders, as a csv:
+
 CSV.write(joinpath("artifacts", "metaweb_gbif.csv"), gbif_cleanup)
+
+# ## Cleaning the phylogeny
+
+# The phylogeny suffers from much the same problem as the European metaweb. We
+# will therefore use much of the same solution to fix the names.
+
+# The first step is to read the tree as a nexus file using `Phylo`, and to get
+# rid of all the internal nodes -- we are only interested in the leaves of the
+# tree.
 
 tree = open(parsenexus, joinpath("data", "mammals.nex"))["*UNTITLED"]
 treenodes = [n.name for n in tree.nodes if !startswith(n.name, "Node ")]
+
+# This step is a little dirty. Remember that data cleaning is a sin eater, and
+# we need to eat other people's data sins at some point. This is this point.
+# Essentially, we will iterate through the rows of the cleaned metaweb names,
+# and match on either `Gen sp` or `Gen_sp`.
+
+# There is an edge case that was not solved automatically, and so the *S.
+# musicus* species gets its correct tree name manually. This name is caught
+# correctly by *e.g.* `NCBITaxonomy.jl`, but this introduced another dependency
+# in the project, and requires another re-harmonization with the GBIF backbone.
 
 n = []
 for row in eachrow(gbif_cleanup)
@@ -58,5 +128,16 @@ for row in eachrow(gbif_cleanup)
     end
 end
 
+# ## Merging the cleaned names together
+
+# Because the positions in `n` match with the row indices in the cleaned metaweb
+# names, we add this object as a column:
+
 gbif_cleanup.upham = n
+
+# We finally save this new file as another artifact. Generally, we will err on
+# the side of caution and save multiple files with redudancy in them. This is
+# good practice, and it allows for easier data dumpster diving if something goes
+# wrong.
+
 CSV.write(joinpath("artifacts", "names_metaweb_tree_gbif.csv"), gbif_cleanup)
