d5fa0697206da3671975191ea8b30a03a56b49b8
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-19 20:33:19 -0400

    🧮 taxonomic overlap between the regions

--- A3_taxon_comparison.jl
+++ A3_taxon_comparison.jl
@@ -17,7 +17,7 @@ list_eur = mammal_europe()
 
 master_list = Dict{String, GBIFTaxon}()
 for tax in string.(list_can ∪ list_eur)
-    master_list[tax] = taxon(tax)
+    master_list[tax] = GBIF.taxon(tax)
 end
 
 genus_can = unique([master_list[s].genus.first for s in list_can])
@@ -45,3 +45,22 @@ for sp in list_can
         congenerics += 1
     end
 end
+
+results = DataFrame(level=String[], can=Int[], common=Int[], eur=Int[], overlap=Float64[])
+push!(results,
+    ("species", length(setdiff(list_can, list_eur)), length(list_can ∩ list_eur), length(setdiff(list_can, list_eur)), length(list_can ∩ list_eur)/length(list_can ∪ list_eur))
+)
+
+push!(results,
+    ("genus", length(setdiff(genus_can, genus_eur)), length(genus_can ∩ genus_eur), length(setdiff(genus_can, genus_eur)), length(genus_can ∩ genus_eur)/length(genus_can ∪ genus_eur))
+)
+
+push!(results,
+    ("family", length(setdiff(family_can, family_eur)), length(family_can ∩ family_eur), length(setdiff(family_can, family_eur)), length(family_can ∩ family_eur)/length(family_can ∪ family_eur))
+)
+
+push!(results,
+    ("order", length(setdiff(order_can, order_eur)), length(order_can ∩ order_eur), length(setdiff(order_can, order_eur)), length(order_can ∩ order_eur)/length(order_can ∪ order_eur))
+)
+
+CSV.write("artifacts/taxonomic_overlap.csv", results)
