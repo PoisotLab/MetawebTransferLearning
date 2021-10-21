d6a61efd1e032cd9deac9e7593b0002b2189852d
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-21 16:30:26 -0400

    🔬 also try with adding links

--- A4_threshold_validation.jl
+++ A4_threshold_validation.jl
@@ -6,6 +6,7 @@ using StatsPlots
 using StatsBase
 using ProgressMeter
 using Random
+using SparseArrays
 
 theme(:mute)
 default(; frame=:box, aspectratio=1, dpi=600)
@@ -28,115 +29,118 @@ end
 
 ℒ, ℛ = rdpg(M, 12)
 
-drop_at = 10:250:(links(M)-1)
-drop_auc = zeros(Float64, length(drop_at))
-drop_acc = zeros(Float64, length(drop_at))
-drop_racc = zeros(Float64, length(drop_at))
-drop_bacc = zeros(Float64, length(drop_at))
-drop_J = zeros(Float64, length(drop_at))
-drop_thr = zeros(Float64, length(drop_at))
-drop_kappa = zeros(Float64, length(drop_at))
-drop_recov = zeros(Float64, length(drop_at))
-drop_L = zeros(Float64, length(drop_at))
-drop_R = zeros(Float64, length(drop_at))
-drop_co = zeros(Float64, length(drop_at))
-drop_rho = zeros(Float64, length(drop_at))
-drop_s4 = zeros(Float64, length(drop_at))
-drop_s5 = zeros(Float64, length(drop_at))
+links_changed = 10:250:(links(M)-1)
 
-
-@showprogress for (dridx, dropint) in enumerate(drop_at)
-    m = copy(M)
-    pool_to_remove = sample(interactions(M), dropint; replace=false)
-    for i in pool_to_remove
-        m[i.from, i.to] = false
-    end
-    L, R = rdpg(m, 12)
-    A = adjacency(M)
-    thresholds = LinRange(extrema(L * R)..., 500)
+function threshold_network(predicted::Matrix{Float64}, observed::Matrix{Bool})
+    thresholds = LinRange(extrema(predicted)..., size(predicted, 1))
     tp = zeros(Float64, length(thresholds))
     tn = similar(tp)
     fp = similar(tp)
     fn = similar(tp)
     for (i, t) in enumerate(thresholds)
-        PN = (L * R) .>= t
-        tp[i] = sum((A) .& (PN)) / sum(A)
-        tn[i] = sum((.!(A)) .& (.!(PN))) / sum(.!(A))
-        fp[i] = sum((.!(A)) .& (PN)) / sum(.!(A))
-        fn[i] = sum((A) .& (.!(PN))) / sum(A)
+        prediction = predicted .>= t
+        tp[i] = sum((observed) .& (prediction)) / sum(observed)
+        tn[i] = sum((.!(observed)) .& (.!(prediction))) / sum(.!(observed))
+        fp[i] = sum((.!(observed)) .& (prediction)) / sum(.!(observed))
+        fn[i] = sum((observed) .& (.!(prediction))) / sum(observed)
     end
     n = tp .+ fp .+ tn .+ fn;
     tpr = tp ./ (tp .+ fn);
     fpr = fp ./ (fp .+ tn);
     acc = (tp .+ tn) ./ (n)
     racc = ((tn .+ fp) .* (tn .+ fn) .+ (fn .+ tp) .* (fp .+ tp)) ./ (n .* n);
-    bacc = ((tp ./ (tp .+ fn)) .+ (tn ./ (fp .+ tn))) ./ 2.0
     J = (tp ./ (tp .+ fn)) + (tn ./ (tn .+ fp)) .- 1.0;
-    ppv = tp ./ (tp .+ fp);
     κ = (acc .- racc) ./ (1.0 .- racc);
     dx = [reverse(fpr)[i] - reverse(fpr)[i - 1] for i in 2:length(fpr)]
     dy = [reverse(tpr)[i] + reverse(tpr)[i - 1] for i in 2:length(tpr)]
     AUC = sum(dx .* (dy ./ 2.0))
-    drop_auc[dridx] = AUC
-    drop_J[dridx] = first(findmax(J))
-    drop_kappa[dridx] = first(findmax(κ))
-    drop_thr[dridx] = thresholds[last(findmax(J))]
-    drop_acc[dridx] = acc[last(findmax(J))]
-    drop_racc[dridx] = racc[last(findmax(J))]
-    drop_bacc[dridx] = bacc[last(findmax(J))]
-    drop_L[dridx] =  mean(sqrt.((L .- ℒ).^2.0))
-    drop_R[dridx] =  mean(sqrt.((R .- ℛ).^2.0))
-    N = UnipartiteQuantitativeNetwork(L*R, species(m)) > thresholds[last(findmax(J))]
-    drop_recov[dridx]= count(!isnothing, indexin(pool_to_remove, interactions(N)))/dropint
-    drop_co[dridx] = connectance(N)
-    drop_rho[dridx] = ρ(N)
-    drop_s4[dridx] = length(find_motif(N, unipartitemotifs()[:S4]))
-    drop_s5[dridx] = length(find_motif(N, unipartitemotifs()[:S5]))
+    best_at = last(findmax(J))
+    return (accuracy=acc[best_at], J=maximum(J), AUC=AUC, κ=κ[best_at], τ=thresholds[best_at])
 end
 
-plot([0, 1, 1, 0], [0, 0, 0.5, 0.5], st=:shape, c=:grey, alpha=0.1, lw=0.0, aspectratio=1)
+function drop_interactions(network, n)
+    m = copy(network)
+    pool_to_remove = sample(findall(isequal(true), m.edges), n; replace=false)
+    m.edges[pool_to_remove] .= false
+    SparseArrays.dropzeros!(m.edges)
+    return m
+end
+
+function add_interactions(network, n)
+    m = copy(network)
+    pool_to_add = sample(findall(isequal(false), m.edges), n; replace=false)
+    m.edges[pool_to_add] .= true
+    return m
+end
+
+
+add_results = []
+rem_results = []
+for n in links_changed
+    m = add_interactions(M, n)
+    t = threshold_network(prod(rdpg(m,12)), adjacency(M))
+    rm = UnipartiteNetwork(prod(rdpg(m, 12)) .>= t.τ, species(m))
+    detected = count(isnothing, indexin(setdiff(interactions(m), interactions(M)), interactions(rm)))
+    push!(add_results, merge(t, (detected=detected, changed=n)))
+end
+
+for n in links_changed
+    m = drop_interactions(M, n)
+    t = threshold_network(prod(rdpg(m,12)), adjacency(M))
+    rm = UnipartiteNetwork(prod(rdpg(m, 12)) .>= t.τ, species(m))
+    detected = count(!isnothing, indexin(setdiff(interactions(M), interactions(m)), interactions(rm)))
+    push!(rem_results, merge(t, (detected=detected, changed=n)))
+end
+
+
+plot([0, 1, 1, 0], [0, 0, 0.5, 0.5], st=:shape, c=:grey, alpha=0.1, lw=0.0, aspectratio=1, legend=:bottomright, lab="")
 annotate!(0.05, 0.25, text("Worse than random", :black, :left, 8))
-plot!([0, 1, 1, 0], [0.5, 0.5, 0.5+1/6, 0.5+1/6], st=:shape, c=:red, alpha=0.1, lw=0.0)
+plot!([0, 1, 1, 0], [0.5, 0.5, 0.5+1/6, 0.5+1/6], st=:shape, c=:red, alpha=0.1, lw=0.0, lab="")
 annotate!(0.05, 0.5+1/12, text("Close to random", :red, :left, 8))
-plot!([0, 1, 1, 0], [0.5+1/6, 0.5+1/6, 0.5+2/6, 0.5+2/6], st=:shape, c=:orange, alpha=0.1, lw=0.0)
+plot!([0, 1, 1, 0], [0.5+1/6, 0.5+1/6, 0.5+2/6, 0.5+2/6], st=:shape, c=:orange, alpha=0.1, lw=0.0, lab="")
 annotate!(0.05, 0.5+3/12, text("Fair classifier", :orange, :left, 8))
-plot!([0, 1, 1, 0], [0.5+2/6, 0.5+2/6, 1.0, 1.0], st=:shape, c=:green, alpha=0.1, lw=0.0)
+plot!([0, 1, 1, 0], [0.5+2/6, 0.5+2/6, 1.0, 1.0], st=:shape, c=:green, alpha=0.1, lw=0.0, lab="")
 annotate!(0.95, 0.5+5/12, text("Excellent classifier", :green, :right, 8))
-scatter!(drop_at./links(M), drop_auc, lab="", leg=false, msw=0.0, ms=3, c=:black)
-xaxis!("Interactions withheld", (0, 1))
+scatter!(links_changed./links(M), [r.AUC for r in add_results], lab="Added links", msw=1.0, c=:white)
+scatter!(links_changed./links(M), [r.AUC for r in rem_results], lab="Removed links", msw=1.0, c=:white, m=:diamond)
+xaxis!("Interactions changed", (0, 1))
 yaxis!("ROC-AUC", (0,1))
 savefig(joinpath(suppfig, "sensibility_rocauc.png"))
 
-plot([0, 1, 1, 0], [0, 0, 0.5, 0.5], st=:shape, c=:grey, alpha=0.2, lw=0.0, aspectratio=1)
+plot([0, 1, 1, 0], [0, 0, 0.5, 0.5], st=:shape, c=:grey, alpha=0.2, lw=0.0, aspectratio=1, lab="", legend=:bottomright)
 annotate!(0.05, 0.25, text("Worse than random", :black, :left, 8))
-scatter!(drop_at./links(M), drop_bacc, lab="", leg=false, msw=0.0, ms=3, c=:black)
+scatter!(links_changed./links(M), [r.accuracy for r in add_results], lab="Added links", msw=1.0, c=:white)
+scatter!(links_changed./links(M), [r.accuracy for r in rem_results], lab="Removed links", msw=1.0, c=:white, m=:diamond)
 annotate!(0.05, 0.75, text("Better than random", :black, :left, 8))
-xaxis!("Interactions withheld", (0, 1))
+xaxis!("Interactions changed", (0, 1))
 yaxis!("Accuracy", (0,1))
 savefig(joinpath(suppfig, "sensibility_accuracy.png"))
 
-plot([0, 1, 1, 0], [0, 0, 0.2, 0.2], st=:shape, c=:grey, alpha=0.5, lw=0.0, aspectratio=1)
+plot([0, 1, 1, 0], [0, 0, 0.2, 0.2], st=:shape, c=:grey, alpha=0.5, lw=0.0, aspectratio=1, legend=:bottomright, lab="")
 annotate!(0.05, 0.1, text("Poor agreement", :black, :left, 8))
-plot!([0, 1, 1, 0], [0.2, 0.2, 0.4, 0.4], st=:shape, c=:grey, alpha=0.4, lw=0.0)
+plot!([0, 1, 1, 0], [0.2, 0.2, 0.4, 0.4], st=:shape, c=:grey, alpha=0.4, lw=0.0, lab="")
 annotate!(0.05, 0.3, text("Fair agreement", :black, :left, 8))
-plot!([0, 1, 1, 0], [0.4, 0.4, 0.6, 0.6], st=:shape, c=:grey, alpha=0.3, lw=0.0)
+plot!([0, 1, 1, 0], [0.4, 0.4, 0.6, 0.6], st=:shape, c=:grey, alpha=0.3, lw=0.0, lab="")
 annotate!(0.05, 0.5, text("Moderate agreement", :black, :left, 8))
-plot!([0, 1, 1, 0], [0.6, 0.6, 0.8, 0.8], st=:shape, c=:grey, alpha=0.2, lw=0.0)
+plot!([0, 1, 1, 0], [0.6, 0.6, 0.8, 0.8], st=:shape, c=:grey, alpha=0.2, lw=0.0, lab="")
 annotate!(0.05, 0.7, text("Good agreement", :black, :left, 8))
 annotate!(0.95, 0.9, text("Very good agreement", :black, :right, 8))
-scatter!(drop_at./links(M), drop_kappa, lab="", leg=false, msw=0.0, ms=3, c=:black)
-xaxis!("Interactions withheld", (0, 1))
+scatter!(links_changed./links(M), [r.κ for r in add_results], lab="Added links", msw=1.0, c=:white)
+scatter!(links_changed./links(M), [r.κ for r in rem_results], lab="Removed links", msw=1.0, c=:white, m=:diamond)
+xaxis!("Interactions changed", (0, 1))
 yaxis!("Cohen's κ", (0,1))
 savefig(joinpath(suppfig, "sensibility_kappa.png"))
 
-scatter(drop_at./links(M), drop_thr, leg=false, msw=0.0, ms=3, c=:black, aspectratio=1)
-hline!([0.206], ls=:dash, c=:grey)
-xaxis!("Interactions withheld", (0, 1))
+scatter(links_changed./links(M), [r.τ for r in add_results], lab="Added links", msw=1.0, c=:white)
+scatter!(links_changed./links(M), [r.τ for r in rem_results], lab="Removed links", msw=1.0, c=:white, m=:diamond)
+hline!([0.206], ls=:dash, c=:grey, lab="")
+xaxis!("Interactions changed", (0, 1))
 yaxis!("Best threshold", (0,1))
 savefig(joinpath(suppfig, "sensibility_threshold.png"))
 
-scatter(drop_at./links(M), drop_recov, leg=false, msw=0.0, ms=3, c=:black, aspectratio=1)
-xaxis!("Interactions withheld", (0, 1))
+scatter(links_changed./links(M), [r.detected/r.changed for r in add_results], lab="Added links", msw=1.0, c=:white, legend=:bottomleft)
+scatter!(links_changed./links(M), [r.detected/r.changed for r in rem_results], lab="Removed links", msw=1.0, c=:white, m=:diamond)
+xaxis!("Interactions changed", (0, 1))
 yaxis!("Interactions recovered", (0,1))
 savefig(joinpath(suppfig, "sensibility_recovery.png"))
 
